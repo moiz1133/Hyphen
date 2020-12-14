3846bb81dba795bb36a39a4a797dfd492d81f4f7
Abdul Moiz Asif <45308052+moiz1133@users.noreply.github.com>
2020-12-14 22:50:06 +0500

    updated taskfule

--- dist/task.js
+++ dist/task.js
@@ -1,2 +1,6 @@
-var msg="My name is ABdul Moiz Asif";
-console.log(msg);
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
